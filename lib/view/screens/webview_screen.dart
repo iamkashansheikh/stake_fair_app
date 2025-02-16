04b2c825fc20ba5e4f76b54386e35547d1b05333
Your Name <your.email@example.com>
2025-02-16 14:07:26 +0500

    start runnig  on emulator

---
+++ lib/view/screens/webview_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://eka247.com/exchange/plus"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea( // ✅ Ensures content is within safe screen area
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
+  }
+}
