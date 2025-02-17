6578899b7ef1a975f3bdc8da4422ee4a482111f2
Your Name <your.email@example.com>
2025-02-17 16:53:02 +0500

    authentication screens done

---
+++ lib/controllers/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  //static const Color buttonColor = Color(0xFF115740);
+  static const Color buttonColor = Colors.amber;
+  static const Color blackColor = Colors.black;
+
+  // final Color commonColor =Colors.black.withOpacity(0.07);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:stake_fair_app/view/screens/home_screen.dart';
-import 'package:stake_fair_app/view/screens/webview_screen.dart';
+
+import 'view/screens/authentication/login_screen.dart';
+import 'view/screens/authentication/sign_up_screen.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+    Size mediaQuerySize = MediaQuery.of(context).size;
+
+    return ScreenUtilInit(
+      designSize: Size(mediaQuerySize.width, mediaQuerySize.height),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+         home: LoginScreen()),
     );
   }
 }
-
 
