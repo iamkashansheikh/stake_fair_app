bffd7ea0b1d1db18552cc3158021e518e38bb149
Kashan Shaikh <kashanshaikh681@gmail.com>
2025-03-18 12:02:30 +0500

    add new classes for handling api response or exceptions

---
+++ lib/data/appexception.dart
@@ -0,0 +1,30 @@
+class AppExceptions implements Exception {
+  final _message;
+  final _prifix;
+  AppExceptions([this._message, this._prifix]);
+
+  @override
+  String toString() {
+    return '$_message$_prifix';
+  }
+}
+
+class InternetExceptions extends AppExceptions {
+  InternetExceptions([String? message]) : super(message, 'No Internet');
+}
+
+class RequestTimeOut extends AppExceptions {
+  RequestTimeOut([String? message]) : super(message, 'Request Time Out');
+}
+
+class ServerExceptions extends AppExceptions {
+  ServerExceptions([String? message]) : super(message, 'Internal Server error');
+}
+
+class InvalidException extends AppExceptions {
+  InvalidException([String? message]) : super(message, 'Invalid Url');
+}
+
+class FetchDataException extends AppExceptions {
+  FetchDataException([String? message]) : super(message, 'Error occured while communicating with server');
+}
