39db8e29cf5e02a8a816ba5c4220ef8f29a5d03b
Kashan Shaikh <kashanshaikh681@gmail.com>
2025-03-24 15:19:54 +0500

    live counts

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:stake_fair_app/res/routs/app_routs.dart';
-import 'package:stake_fair_app/res/routs/app_routs_name.dart';
 import 'package:stake_fair_app/view/screens/app_Loacalization/app_local.dart';
 import 'package:stake_fair_app/view/screens/inplay_screen.dart';
 import 'firebase_options.dart';
