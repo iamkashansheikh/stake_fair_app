2ac24f3a3206d0e579ec86a88388687bebaaa662
Kashan Shaikh <kashanshaikh681@gmail.com>
2025-05-19 15:18:01 +0500

    changes

--- lib/repositroy/home_repository/home_repository.dart
+++ lib/repositroy/home_repository/home_repository.dart
@@ -9,18 +9,9 @@ class HomeRepository {
     return await _apiServices.postApi(data, url);
   }
 
-  Future<dynamic> racingEventsListApi(dynamic data) async {
-    final String url = AppUrls.baseUrl + AppUrls.racingEventsListApi;
-    return await _apiServices.postApi(data, url);
-  }
-
   Future<dynamic> sportListApi(dynamic data) async {
     final String url = AppUrls.baseUrl2 + AppUrls.sportList;
     return await _apiServices.postApi(data, url);
   }
 
-  //   Future<dynamic> alleventlist(dynamic data)async{
-  //   final String url = AppUrls.baseUrl + AppUrls.allevenlist;
-  //   return await _apiServices.postApi(data, url);
-  // }
 }
